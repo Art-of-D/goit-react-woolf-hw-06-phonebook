fc8ab181e3eb2aee5183ed497f22c7f15aea0e15
Artur Didur <didur.art@gmail.com>
2024-03-11 10:59:52 +0100

    light

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,34 +2,11 @@ import { Filter } from './Phonebook/Filter/Filter';
 import { Section } from './Phonebook/Section/Section';
 import { ContactForm } from './Phonebook/ContactForm/ContactForm';
 import { ContactList } from './Phonebook/ContactList/ContactList';
-
-//import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { getContacts } from 'reduxStore/selectors';
 
 export const App = () => {
-  // const KEY_WORD = 'contacts';
   const { contacts } = useSelector(getContacts);
-
-  //const [contacts, setContacts] = useState(
-  //   () => JSON.parse(localStorage.getItem(KEY_WORD)) || []
-  // );
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem(KEY_WORD, JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const addContact = ({ name: newName, number }) => {
-
-  //   const newContact = {
-  //     id: uuidv1(),
-  //     name: newName,
-  //     number: number,
-  //   };
-
-  //   setContacts(prevContacts => [...prevContacts, newContact]);
-  // };
 
   return (
     <>
